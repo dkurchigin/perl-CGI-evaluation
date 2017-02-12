6a23dc803c868d03e25dd00d4fec5d7ca3f8a703
kurchigin.dmitry <kurchigin.dmitry@yandex.ru>
2017-02-12 14:57:38 +0000

    add registration

--- registration.pl
+++ registration.pl
@@ -11,32 +11,46 @@ my $user = "root";
 my $sql_passwd = '2NB5n$0&XTEU';
 my $db = "evaluation";
 
-my $dbh = DBI->connect("DBI:mysql:$db:$host:$port",$user,$sql_passwd);
-my $sth = $dbh->prepare("select * from users"); 
-$sth->execute; 
-
-open(my $fh, '>>', '/home/pi/fcgi/testing');
-while ( my $ref = $sth->fetchrow_arrayref) {
-print $fh "$$ref[0]\t"; 
-print $fh "$$ref[1]\t";  
-print $fh "$$ref[3]\n"; 
-} 
-close $fh;
-
-my $rc = $sth->finish;    
-$rc = $dbh->disconnect;
-
 if (param) {
 	my $fio = param('fio');
 	my $login = param('login');
 	my $passwd = param('passwd');
 	my $checkpasswd = param('checkpasswd');
 	my $department = param('department');
+
 	if ($passwd != $checkpasswd) {
-	print redirect('http://192.168.8.5/registration/mismatch.html');	
+		print redirect('http://192.168.8.5/registration/mismatch.html');	
+	} elsif (checkUser($login)) {
+		 print redirect('http://192.168.8.5/registration/already_created.html');	
 	} else {
-		open(my $fh, '>>', '/home/pi/fcgi/testing');
-		print $fh "$fio $login $passwd $checkpasswd $department \n";
-		print redirect('http://192.168.8.5/registration/success.html');
-	}	 
+		addUser($login, $passwd, $department);
+		print redirect('http://192.168.8.5/registration/success.html'); 
+	}
+}
+
+sub checkUser {
+	my $dbh = DBI->connect("DBI:mysql:$db:$host:$port",$user,$sql_passwd);
+	my $sth = $dbh->prepare("select * from users");
+	$sth->execute;
+
+	while (my $ref = $sth->fetchrow_arrayref) {
+		if ($$ref[0] =~ /@_/) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	my $rc = $sth->finish;
+	$rc = $dbh->disconnect;
+}
+
+sub addUser {
+	my $dbh = DBI->connect("DBI:mysql:$db:$host:$port",$user,$sql_passwd); 		my $sth = $dbh->prepare("insert into users (login, passwd, vote, department) value (?, ?, 'N', ?)");
+        
+	my ($login, $passwd, $department) = @_;
+	$sth->execute($login, $passwd, $department);
+        
+	my $rc = $sth->finish;
+        $rc = $dbh->disconnect;
 }
